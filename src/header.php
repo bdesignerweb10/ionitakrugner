0568600692c1adb3f3d99d908ec77c18c0bb845e
BRUNO GOMES DA SILVA <201106@CLARETIANO.REDE>
2019-06-04 17:06:47 -0300

    first commit

---
+++ src/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="description" content="Ionita Krugner" />
+	<meta name="keywords" content="Advogado, Ionita, Krugner, Mulher, Direitos" />
+	<meta name="author" content="Bruno Gomes"/>
+
+	<meta name="robots" content="index, follow" />
+	<meta name="googlebot" content="index, follow" />
+	
+	<title>Ionita Krugner</title>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+	<link rel="manifest" href="img/favicon/manifest.json">
+	<link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="img/favicon/browserconfig.xml">
+	<!--<meta name="theme-color" content="#ffffff">-->	
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-toggle.min.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.datetimepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/textext.plugin.autocomplete.css">
+	<link rel="stylesheet" type="text/css" href="css/lightbox.min.css">	
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">	
+</head>
+<body>
+	<header class="header">
+		<div class="row header-menu">
+			<div class="col-sm-10"></div>
+			<div class="col-sm-2 social">
+				<a href=""><i class="fab fa-facebook-f"></i></a>
+				<a href=""><i class="fab fa-instagram"></i></a>
+				<a href=""><i class="fab fa-twitter"></i></a>
+				<a href=""><i class="fab fa-youtube"></i></a>
+			</div>
+		</div><!-- header-menu-->
+		<div class="row menu">
+			<div class="col-sm-4 logo">
+				<h2><strong>Ionita</strong> Krügner</h2>
+			</div>
+			<div class="col-sm-8">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light nav-menu">
+		  <a class="navbar-brand" href="#"></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="index.php">Inicio</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Perfil</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Serviços</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Entrevistas</a>
+		      </li>		      
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Informativos</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Jurisprudência</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link nav-blog" href="#">Blog</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contato</a>
+		      </li>
+		    </ul>		    
+		  </div>
+		</nav>
+			</div>
+		</div><!--row -->
+	</header>
+	
+	
+
